e8d1925e20f38483f5bad06a65d1b8e48a0c7c5a
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-03-06 17:48:47 +0600

    get single user info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,30 +11,37 @@ app.use(express.json());
 
 // mongo
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kusbv.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const usersCollection = client.db("chitchat-v1").collection("users");
-         //insert every new user
-        app.post("/storeUserInfo", async (req, res) => {
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-
-    }
-
-    catch{
-
-    }
+async function run() {
+  try {
+    const usersCollection = client.db("chitchat-v1").collection("users");
+    //insert every new user
+    app.post("/storeUserInfo", async (req, res) => {
+      const user = req.body;
+      user["profilePhoto"] = "https://i.ibb.co/RvdV5Kx/default-Avatar.webp";
+      user["coverPhoto"] = "https://i.ibb.co/RvdV5Kx/default-Avatar.webp";
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    //get getSingleUserInfo
+    app.get("/getSingleUserInfo/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+  } catch {}
 }
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
-
-app.get('/', (req,res)=>{
-    res.send('ChitChat api found')
-})
-app.listen(port, ()=>{
-    console.log('server running')
-})
+app.get("/", (req, res) => {
+  res.send("ChitChat api found");
+});
+app.listen(port, () => {
+  console.log("server running");
+});
